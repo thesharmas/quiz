79efb539a1370589e22a359685728066d1779e32
Rohit <rnsharma@gmail.com>
2022-02-20 21:00:17 -0800

    usestate

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import QuestionCard from './QuestionCard';
 import Stop from './Stop';
+import React, { useState } from 'react';
+
 function App() {
   let title="Emergency Fund";
   let text="Do you have an emergency fund setup?"
+  const [visible, noClick] = useState(0);
+
+  console.log("visible",visible)
   
   return (
     <div className="App">
       
-      <QuestionCard questionTitle={title} questionText={text}/>
-     
-      <Stop title={title}/>
+      <QuestionCard questionTitle={title} questionText={text} yesClick={noClick}/>
+      {visible ? <Stop title={title}/> : null}
     </div>
   );
 }
--- src/QuestionCard.js
+++ src/QuestionCard.js
@@ -1,24 +1,26 @@
 function QuestionCard(props){
+    console.log(props.yesClick)
+    console.log(props)
     return(
-        <div class="max-w-2xl mx-auto sm:px-6 lg:px-8">
-        <div class="overflow-hidden shadow-md">
+        <div className="max-w-2xl mx-auto sm:px-6 lg:px-8">
+        <div className="overflow-hidden shadow-md">
     
-        <div class="px-6 py-4 bg-white border-b border-gray-200 font-bold uppercase">
+        <div className="px-6 py-4 bg-white border-b border-gray-200 font-bold uppercase">
            {props.questionTitle}
         </div>
 
        
-        <div class="p-6 bg-white border-b border-gray-200">
+        <div className="p-6 bg-white border-b border-gray-200">
             {props.questionText}
         </div>
 
      
-        <div class="flex justify-between  p-6 bg-white border-gray-200 text-center">
+        <div className="flex justify-between space-x-40 p-6 bg-white border-gray-200 text-center">
            
-        <a class="bg-blue-500 order-1 basis-4 shadow-md text-sm text-white font-bold py-3 md:px-8 px-4 hover:bg-blue-400 rounded uppercase" 
-                href="#">Yes</a>
-        <a class="bg-blue-500 order-2 basis-4 shadow-md text-sm text-white font-bold py-3 md:px-8 px-4 hover:bg-blue-400 rounded uppercase" 
-                href="#">No</a>
+        <button onClick={() => props.yesClick(1)} className="bg-blue-500 order-1 basis-4 shadow-md text-sm text-white font-bold py-3 md:px-8 px-4 hover:bg-blue-400 rounded uppercase" 
+                >Yes</button>
+        <button  className="bg-blue-500 order-2 basis-4 shadow-md text-sm text-white font-bold py-3 md:px-8 px-4 hover:bg-blue-400 rounded uppercase" 
+                >No</button>
         </div>
      </div>
     </div>
